aee938725d1fbc36799a685462c4aa36262bba1e
N0RD17 <82229220+N0RD17@users.noreply.github.com>
2024-10-05 18:29:02 +0000

    Updated main.c to be more implicit
    Added comments.
    Changed zbuffer reset to use memset instead of -INFINITY
    Made the rect have a default value of 1 (since it wouldn't matter if it's 8 or 1 or 5)

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 
 #define width 640 / 2
@@ -50,6 +51,7 @@ float calculateZ(int i, int j, int k)
         cos(B) + i * sin(B);
 }
 
+// Calculate each Cube's Surface
 void calculateForSurface(float cubeX, float cubeY, float cubeZ, SDL_Rect *rect, SDL_Color* color)
 {
     x = calculateX(cubeX, cubeY, cubeZ);
@@ -67,9 +69,10 @@ void calculateForSurface(float cubeX, float cubeY, float cubeZ, SDL_Rect *rect, 
         {
             zBuffer[idx] = ooz;
 
-            // Update rect position for each point
+            // Update rect position for each point.
             rect->x = xp;
             rect->y = yp;
+            // Apply values to the current index.
             buffer[idx].rect = *rect;
             buffer[idx].color = *color;
         }
@@ -78,27 +81,32 @@ void calculateForSurface(float cubeX, float cubeY, float cubeZ, SDL_Rect *rect, 
 
 int main(void)
 {
+    // Initializing SDL2
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "SDL_INIT ERROR: %s\n", SDL_GetError());
         return -1;
     }
 
+    // Creating a Window.
     SDL_Window* window = SDL_CreateWindow("Pixeler", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
     if(window == NULL) {
         fprintf(stderr, "SDL_CreateWindow ERROR: %s\n", SDL_GetError());
         return -1;
     }
 
+    // Creating a renderer for rendering inside the window.
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if(window == NULL) {
         fprintf(stderr, "SDL_CreateRenderer ERROR: %s\n", SDL_GetError());
         return -1;
     }
 
+    // The main rectangle that will be modified for each pixel.
     SDL_Rect RectPix;
-    RectPix.w = 8;
-    RectPix.h = 8;
+    RectPix.w = 1;
+    RectPix.h = 1;
 
+    // Colors
     SDL_Color RedColor    = {.r=0xFF, .g=0x00, .b=0x00, .a=0xFF};
     SDL_Color GreenColor  = {.r=0x00, .g=0xFF, .b=0x00, .a=0xFF};
     SDL_Color BlueColor   = {.r=0x00, .g=0x00, .b=0xFF, .a=0xFF};
@@ -107,8 +115,11 @@ int main(void)
     SDL_Color TealColor   = {.r=0x00, .g=0xFF, .b=0xFF, .a=0xFF};
     
 
+    // Loop logic and events
     SDL_bool run = SDL_TRUE;
     SDL_Event event;
+    
+    // Window-Render Loop
     while(run)
     {
         while(SDL_PollEvent(&event))
@@ -123,19 +134,21 @@ int main(void)
             }
         }
 
-//        memset(zBuffer, 0, width * height * 4);
+        // Clears the buffer to properly re-draw the screen.
+        memset(zBuffer, 0, width * height * 4);
         for(int i = 0; i < width * height; i++)
         {
-            zBuffer[i] = -INFINITY;
             buffer[i].rect.x = buffer[i].rect.y = buffer[i].rect.w =
                 buffer[i].rect.h = 0;
             buffer[i].color.r = buffer[i].color.g = buffer[i].color.b =
                 buffer[i].color.a = 0;
         }
 
+        // Clears the background so it is black
         SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
         SDL_RenderClear(renderer);
 
+        // Handles Math for Spinning Cube
         for(float cubeX = -cubeWidth; cubeX < cubeWidth; cubeX += incrementSpeed)
         {
             for(float cubeY = -cubeWidth; cubeY < cubeWidth; cubeY += incrementSpeed)
@@ -155,12 +168,14 @@ int main(void)
             }
         }
 
+        // Draws the specific index attributes onto the renderer
         for(int k = 0; k < width * height; k++)
         {
             SDL_SetRenderDrawColor(renderer, buffer[k].color.r, buffer[k].color.g, buffer[k].color.b, buffer[k].color.a);
             SDL_RenderFillRect(renderer, &buffer[k].rect);
         }
 
+        // Displays the renderer contents
         SDL_RenderPresent(renderer);
 
         A += 0.005;
@@ -168,8 +183,11 @@ int main(void)
 //        C += 0.005; // Hurts THE EYES
     }
 
+    // Free Resources
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+
+    // Quit out of SDL2 and its SubSystems/Free SubSystem resources if there's any.
     SDL_Quit();
     return 0;
 }
